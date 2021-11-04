7673cc22b94c61ab996f8dcd69a643415ed2b3d6
Chris <co.benavides86@gmail.com>
2021-11-04 18:07:54 -0500

    main

--- datos.php
+++ datos.php
@@ -2,22 +2,31 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-    $arr = array('recibido' => $_GET['prueba']);
-    echo json_encode($arr);
 
 $client = new MongoDB\Client(
-    'mongodb+srv://yo:abc13579@cluster0.bosd3.mongodb.net/MultimediaS?retryWrites=true&w=majority');
+    'mongodb+srv://yo:abc13579@cluster0.bosd3.mongodb.net/water?retryWrites=true&w=majority');
 
-$tb=$client->MultimediaS->puntaje;
+$tb=$client->water->Precipitaciones;
+$filter= ['$and'=>
+            [
+                ['ANNO'=>['$eq'=>'1997']],
+                ['ESTACION'=>['$eq'=>'Sutatausa']]
+            ]
+        ];
 
-$registro=array(
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
+/*$registro=array(
 "iduser"=>"1458",
 "fecha"=>"1/1/2033",
 "puntaje"=>"14589"
-);
+);*/
 
-$resultado=$tb->insertOne($registro);
-echo $resultado->getInsertedCount();
+//$resultado=$tb->insertOne($registro);
+//echo $resultado->getInsertedCount();
+
+
 
 
 ?>
